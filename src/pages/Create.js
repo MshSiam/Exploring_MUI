ce7d3ecd5d51ed1624ca1f5522f84f6e9b7fc460
MshSiam <siyamkenway@gmail.com>
2022-06-27 01:28:21 +0600

    typography coovered

---
+++ src/pages/Create.js
@@ -0,0 +1,27 @@
+import { Typography } from "@mui/material"
+import React from "react"
+
+const Create = () => {
+  return (
+    <div>
+      <Typography variant="h3" color="primary" align="center" gutterBottom>
+        Create A New Note
+      </Typography>
+      <Typography color="secondary" gutterBottom>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est, adipisci,
+        et dicta sunt, labore necessitatibus eligendi ipsa veritatis ipsam harum
+        sequi reiciendis aperiam earum culpa quos eum enim minima repudiandae?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem illo ex
+        asperiores, voluptatem architecto quo, omnis magnam minima error
+        doloribus molestias officiis. Culpa aspernatur atque incidunt et natus,
+        sint sapiente.
+      </Typography>
+
+      <Typography variant="h6" component="h4" color="textSecondery">
+        Another Notes
+      </Typography>
+    </div>
+  )
+}
+
+export default Create
---
+++ src/pages/Note.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Note = () => {
+  return (
+    <div>
+      <h1>This is note </h1>
+    </div>
+  )
+}
+
+export default Note
